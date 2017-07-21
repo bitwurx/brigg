c4522c3e660ec6e5e0a78574f46daea8fdc64194
Jared Patrick <japatric@cisco.com>
2017-07-21 11:18:00 -0400

    emptied code from main.go

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"fmt"
+	"log"
+
+	"github.com/satori/go.uuid"
+)
+
+var (
+	privateKey *rsa.PrivateKey
+	nodeId     uuid.UUID
+)
+
+func init() {
+	var err error
+
+	nodeId = uuid.NewV1()
+	log.Println(fmt.Sprintf("Node id: %s", nodeId.String()))
+
+	log.Println("Generating rsa key pair...")
+	privateKey, err = rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 
 }
